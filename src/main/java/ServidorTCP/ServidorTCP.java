df9e5c3e7015068eab0a893ec6172a70aea4057f
Bayron <b.munooz15@ufromail.cl>
2020-09-18 15:06:58 -0400

    Crear método main ServidorTCP
    Método que permite la ejecución del servidorTCP, de momento se le agrega un mensaje de bienvenida

--- src/main/java/ServidorTCP/ServidorTCP.java
+++ src/main/java/ServidorTCP/ServidorTCP.java
@@ -1,4 +1,11 @@
 package ServidorTCP;
 
 public class ServidorTCP {
+
+    //Metodo para ejecutar servidor
+    public static void main(String[] args) {
+        //Imprime un mensaje
+        System.out.println("Bienvenido al servidor TCP");
+        
+    }
 }
