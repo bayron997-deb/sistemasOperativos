fe1ec145650124dafda4056f7bca7468e0bf0c88
Bayron <b.munooz15@ufromail.cl>
2020-09-18 18:41:52 -0400

    Agregar instruccion a escribirFichero

--- src/main/java/GestorArchivo/GestorArchivos.java
+++ src/main/java/GestorArchivo/GestorArchivos.java
@@ -1,8 +1,6 @@
 package GestorArchivo;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class GestorArchivos {
     //Atributos
@@ -22,7 +20,27 @@ public class GestorArchivos {
     private File file;
 
     //Metodos
-    public void escribirFichero(){
+
+    /**
+     * Metodo para escribir un archivo en binario
+     * @param ruta ruta que se le pasa al escribidor
+     */
+    public void escribirFichero(String ruta,String contenido){
+        //crea un nuevo arhivo con nombre ruta
+        file = new File(ruta);
+        //Try-Catch para evitar errores
+        try{
+            //Creamos objeto FileOutputStream pasandole file como parametro
+            fos = new FileOutputStream(file);
+            //creamos objeto ObjectOutputStream pasandole a fos como parametro
+            oos = new ObjectOutputStream(fos);
+            //Escribir
+            oos.writeByte(Integer.parseInt(contenido));
+        } catch (FileNotFoundException e) {
+            //errores
+        } catch (IOException e) {
+            //errores
+        }
 
     }
 }
