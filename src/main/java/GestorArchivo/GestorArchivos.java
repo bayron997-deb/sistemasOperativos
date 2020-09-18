69821cd50d8c0d291b4789f2a5cc3ee35a917bef
Bayron <b.munooz15@ufromail.cl>
2020-09-18 18:23:11 -0400

    Atributos para trabajar con ficheros binarios

--- src/main/java/GestorArchivo/GestorArchivos.java
+++ src/main/java/GestorArchivo/GestorArchivos.java
@@ -1,4 +1,28 @@
 package GestorArchivo;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 public class GestorArchivos {
+    //Atributos
+    /**
+     * Escribir bytes en un fichero
+     */
+    private FileOutputStream fos;
+
+    /**
+     * convierte en objetos y variables los array de bytes leidos de un imputstream
+     */
+    private ObjectOutputStream oos;
+
+    /**
+     * Representacion abstracta de nombre de ruta de archivos y directorios
+     */
+    private File file;
+
+    //Metodos
+    public void escribirFichero(){
+
+    }
 }
