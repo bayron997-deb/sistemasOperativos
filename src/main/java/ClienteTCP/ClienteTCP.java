d53e5a1e7f4ec40995899086a875d6e481b57d98
Bayron <b.munooz15@ufromail.cl>
2020-09-18 15:50:33 -0400

    Crear Atributos en ClienteTCP
    teclado tipo BufferedReader ; newSocket tipo socket ; newSalida tipo DataOutpuStream

--- src/main/java/ClienteTCP/ClienteTCP.java
+++ src/main/java/ClienteTCP/ClienteTCP.java
@@ -1,8 +1,23 @@
 package ClienteTCP;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class ClienteTCP {
     ///Atributos
-
+    /**
+     *Leer texto en una secuencia y la almacena en un buffer de caracteres
+     */
+    private BufferedReader teclado;
+    /**
+     * Socket nos permitira recibir/leer datos y para enviar/escribir datos
+     */
+    private Socket newSocket;
+    /**
+     * Escribir datos del tipo primitivo
+     */
+    private DataOutputStream newSalida;
     /**
      * Metodo para ejecutar cliente
      * @param args
