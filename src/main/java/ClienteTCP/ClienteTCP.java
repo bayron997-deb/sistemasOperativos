adcf4413ef47fdc93f4a8c24a1e368377ed3711f
Bayron <b.munooz15@ufromail.cl>
2020-09-18 15:45:21 -0400

    Crear método main ClienteTCP

--- src/main/java/ClienteTCP/ClienteTCP.java
+++ src/main/java/ClienteTCP/ClienteTCP.java
@@ -1,4 +1,14 @@
 package ClienteTCP;
 
 public class ClienteTCP {
+    ///Atributos
+
+    /**
+     * Metodo para ejecutar cliente
+     * @param args
+     */
+    public static void main(String[] args) {
+        //mensaje de bienvenida
+        System.out.println("Bienvenido cliente");
+    }
 }
