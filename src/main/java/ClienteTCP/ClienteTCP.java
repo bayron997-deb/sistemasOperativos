d725ad3a31de16feca9d1c67c9aa26cadca353ca
Bayron <b.munooz15@ufromail.cl>
2020-09-18 15:52:31 -0400

    Nuevo Metodo clienteTCP

--- src/main/java/ClienteTCP/ClienteTCP.java
+++ src/main/java/ClienteTCP/ClienteTCP.java
@@ -10,14 +10,17 @@ public class ClienteTCP {
      *Leer texto en una secuencia y la almacena en un buffer de caracteres
      */
     private BufferedReader teclado;
+
     /**
      * Socket nos permitira recibir/leer datos y para enviar/escribir datos
      */
     private Socket newSocket;
+
     /**
      * Escribir datos del tipo primitivo
      */
     private DataOutputStream newSalida;
+
     /**
      * Metodo para ejecutar cliente
      * @param args
@@ -25,5 +28,12 @@ public class ClienteTCP {
     public static void main(String[] args) {
         //mensaje de bienvenida
         System.out.println("Bienvenido cliente");
+    }
+
+    /**
+     * Metodo de procedimientos de cliente
+     */
+    public void clienteTCP(){
+        
     }
 }
